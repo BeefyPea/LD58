3aa266b5367762f3063ff6c87c7760790af91eac
pdues <pittduester@web.de>
2025-10-04 16:39:30 +0200

    basic enemy movement + music

--- ld-58-game/scripts/pickup.gd
+++ ld-58-game/scripts/pickup.gd
@@ -3,8 +3,9 @@ extends Area2D
  
 @export var item: Itemclass
 
-func _on_body_entered(body):
+func _on_body_entered(body: CharacterBody2D) -> void:
 	# Assuming your player is in the "player" group.
-	if body.is_in_group("player"):
-		Handmanager.add_item(item)
-		queue_free() # The item disappears after being picked up.
+	print("collision")
+	#if body.is_in_group("player"):
+		#Handmanager.add_item(item)
+		#queue_free() # The item disappears after being picked up.
