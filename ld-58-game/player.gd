1cc0e74d37c815283ceb4ec03dbc2d21197db8ca
BenediktPeter <benedikt.peter.zapf@gmail.com>
2025-10-04 11:42:53 +0200

    basic movement

---
+++ ld-58-game/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var speed = 750
+@export var gravity = 2000
+@export var jump_speed = -800
+@export_range(0.0, 1.0) var friction = 0.3
+@export_range(0.0 , 1.0) var acceleration = 0.1
+
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	var dir = Input.get_axis("links", "rechts")
+	if dir != 0:
+		velocity.x = lerp(velocity.x, dir * speed, acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, friction)
+
+	move_and_slide()
+	if Input.is_action_just_pressed("oben"):# and is_on_floor():
+		velocity.y = jump_speed
