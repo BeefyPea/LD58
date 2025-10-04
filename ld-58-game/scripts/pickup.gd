b2e5a4f5f87da7bd2717c63c348d970743f0e42b
BenediktPeter <benedikt.peter.zapf@gmail.com>
2025-10-04 22:59:36 +0200

    boindik kocht

--- ld-58-game/scripts/pickup.gd
+++ ld-58-game/scripts/pickup.gd
@@ -1,8 +1,9 @@
 # pickup
 extends Area2D
- 
-@export var item1 = Item.new()
+var entered = false 
 
+@export var item1 = Item.new()
+@onready var raycast: RayCast2D = $RayCast2D
 func _ready():
 	item1.itemname = "Test Item"
 	item1.description = "A basic test item."
@@ -14,8 +15,10 @@ func init(item) -> void:
 	item1.texture = item.texture
 	
 	# Use item1 as needed
-func _on_body_entered(_body: CharacterBody2D) -> void:
-	if Handmanager.Items.size() < 2 and Input.is_key_pressed(KEY_E) == true:
+
+ 
+func _physics_process(_delta: float) -> void:
+	if Handmanager.Items.size() < 2 and Input.is_key_pressed(KEY_E) == true and raycast.is_colliding() == true :
 		# Assuming your player is in the "player" group.
 		Handmanager.addItem(item1)
 		queue_free() # The item disappears after being picked up.
