6c18c47411b556c7509728298eca54129ddf1f9d
pdues <pittduester@web.de>
2025-10-06 22:58:55 +0200

    boss is working

--- ld-58-game/scripts/enemy.gd
+++ ld-58-game/scripts/enemy.gd
@@ -12,6 +12,7 @@ const max_hp = 5
 var hp = max_hp
 
 @onready var enemy: Node2D = $"."
+#@onready var sprite_2d: AnimatedSprite2D = $"../enemy2/Sprite2D"
 
 @onready var ray_cast_left: RayCast2D = $RayCast_left
 @onready var ray_cast_right: RayCast2D = $RayCast_right
@@ -21,23 +22,18 @@ var hp = max_hp
 @onready var ray_casts_right_aggro: Array = $aggro_ray_cast/ray_casts_right.get_children()
 @onready var ray_casts_left_aggro: Array = $aggro_ray_cast/ray_casts_left.get_children()
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if (velocity < 40):
 		velocity += acceleration * SPEED * delta
 	position.x += 1 * velocity * acceleration * direction * delta
 	if ray_cast_right.is_colliding():
-		velocity = 0
 		direction = -1
 	if ray_cast_left.is_colliding():
-		velocity = 0
 		direction = 1
 	if ray_cast_bottom_right.is_colliding() == false:
-		velocity = 0
 		direction = -1
 	if ray_cast_bottom_left.is_colliding() == false:
-		velocity = 0
 		direction = 1
 	for i in ray_casts_right_aggro:
 		if i.is_colliding():
--- ld-58-game/scripts/game_manager.gd
+++ ld-58-game/scripts/game_manager.gd
@@ -4,6 +4,8 @@ extends Node
 @onready var timer: Timer = $death_timer
 @onready var player: CharacterBody2D = $"../scene_objects/Player"
 @onready var flash_animation: AnimationPlayer = $"../scene_objects/Player/flash_animation"
+@onready var countdown_label: Label = $"../UI/countdown_label"
+@onready var countdown: Timer = $"../UI/countdown"
 
 var hp = 3
 var iframes = false
@@ -28,6 +30,9 @@ func decrease_health():
 	else:
 		pass
 		
+func _process(delta: float) -> void:
+	countdown_label.set_text(str(countdown.get_time_left()).pad_decimals(0))
+		
 func _on_timer_timeout():
 		Engine.time_scale = 1.0
 		get_tree().reload_current_scene()
@@ -36,3 +41,4 @@ func _on_timer_timeout():
 func _on_hitbox_punch_area_entered(area):
 	if area.is_in_group("hurtbox"):
 		area.take_dmg(area)
+	
